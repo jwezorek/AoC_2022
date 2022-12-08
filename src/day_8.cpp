06966b3a2a7b9b493ead5980936d90332a68ed0c
Joe Wezorek <jwezorek@gmail.com>
2022-12-08 09:17:00 -0800

    in a more functional style

--- src/day_8.cpp
+++ src/day_8.cpp
@@ -43,66 +43,10 @@ namespace{
 
     using grid_loc_set = std::unordered_set<grid_loc, hash_grid_loc, grid_loc_equal>;
 
-    auto rows(const grid& ary) {
-        return rv::enumerate(rv::all(ary)) |
-            rv::transform(
-                [](auto&& pair) {
-                    auto [row_index, row] = pair;
-                    return rv::enumerate(rv::all(row)) |
-                        rv::transform(
-                            [row_index](auto&& p)->grid_loc {
-                                auto [col_index, val] = p;
-                                return {
-                                    val,
-                                    static_cast<int>(col_index),
-                                    static_cast<int>(row_index)
-                                };
-                            }
-                            );
-                }
-                );
-    }
-
-    auto all_grid_locs(const grid& ary) {
-        return rows(ary) | rv::join;
-    }
-
-    auto nth_column(const grid& ary, int n) {
-        return rv::all(ary) |
-            rv::transform(
-                [n](const auto& row) {
-                    return row.at(n);
-                }
-        );
-    }
-
-    auto columns(const grid& ary) {
-        return rv::iota(0, static_cast<int>(ary[0].size())) |
-            rv::transform(
-                [&ary](auto col_index) {
-                    return rv::enumerate(nth_column(ary, col_index)) |
-                        rv::transform(
-                            [col_index](auto p)->grid_loc {
-                                auto [row_index, val] = p;
-                                return {
-                                    val,
-                                    static_cast<int>(col_index),
-                                    static_cast<int>(row_index)
-                                };
-                            }
-                    );
-                }
-        );
-    }
-
     bool is_in_grid(const grid& ary, int col, int row) {
         int columns = static_cast<int>(ary[0].size());
         int rows = static_cast<int>(ary.size());
         return row >= 0 && row < rows&& col >= 0 && col < columns;
-    }
-
-    auto running_max(auto rng) {
-        return rv::partial_sum(rng, [](int lhs, int rhs) { return std::max(lhs, rhs); });
     }
 
     auto line(const grid& ary, int c, int r, int col_offset, int row_offset) {
@@ -120,14 +64,46 @@ namespace{
                     return is_in_grid(ary, col, row);
                 }
             ) | rv::transform(
-                [&ary](auto&& c_r) {
+                [&ary](auto&& c_r)->grid_loc {
                     auto [col, row] = c_r;
-                    return ary[row][col];
+                    return {
+                        ary[row][col],
+                        col,
+                        row
+                    };
                 }
-                );
+            );
     }
 
-    auto visible_from_front(auto rng) {
+    auto rows(const grid& ary) {
+        int n = static_cast<int>(ary.size());
+        return rv::iota(0, n) |
+            rv::transform(
+                [&ary](int row_index) {
+                    return line(ary, 0, row_index, 1, 0);
+                }
+            );
+    }
+
+    auto columns(const grid& ary) {
+        int n = static_cast<int>(ary[0].size());
+        return rv::iota(0, n) |
+            rv::transform(
+                [&ary](int col_index) {
+                    return line(ary, col_index, 0, 0, 1);
+                }
+        );
+    }
+
+    auto all_grid_locs(const grid& ary) {
+        return rows(ary) | rv::join;
+    }
+
+    auto running_max(auto rng) {
+        return rv::partial_sum(rng, [](int lhs, int rhs) { return std::max(lhs, rhs); });
+    }
+
+    auto visible(auto rng) {
         return rv::zip(
                 rv::concat(rv::single(-1), running_max(rng | rv::transform([](auto&& loc) {return loc.value; }))),
                 rng
@@ -146,46 +122,25 @@ namespace{
             );
     }
 
-    auto visible(auto rng) {
-        return rv::concat(
-            visible_from_front(rng),
-            visible_from_front(rv::reverse(rng))
-        );
-    }
-
     int num_visible(const grid& ary) {
-        grid_loc_set visible_set;
-        for (auto col : columns(ary)) {
-            for (auto loc : visible(col)) {
-                visible_set.insert(loc);
-            }
-        }
-        for (auto row : rows(ary)) {
-            for (auto loc : visible(row)) {
-                visible_set.insert(loc);
-            }
-        }
+        auto visible_set = rv::concat(rows(ary), columns(ary)) |
+            rv::transform([](auto rng) {
+                return rv::concat(
+                        visible(rng),
+                        visible(rv::reverse(rng))
+                    ); 
+                }
+            ) |
+            rv::join |
+            r::to<grid_loc_set>();
         return static_cast<int>(visible_set.size());
     }
-
-    /*
-    int num_visible(const grid& ary) {
-        auto visi_cols = columns(ary) | rv::transform([](auto rng) {return visible(rng); });
-        
-         | rv::transform(
-            [](auto&& rng) {
-                return visible(rng);
-            }
-        ) | rv::join | r::to< grid_loc_set>();
-       
-        return 0;
-    }
-    */
 
     int viewing_distance(const grid& ary, int col, int row, int col_offset, int row_offset) {
         int max_height = ary[row][col];
         int count = 0;
-        for (int v : line(ary, col, row, col_offset, row_offset) | rv::drop(1)) {
+        for (int v : line(ary, col, row, col_offset, row_offset) | 
+                rv::drop(1) | rv::transform([](const auto& loc) {return loc.value; })) {
             count++;
             if (v >= max_height) {
                 return count;
@@ -206,9 +161,7 @@ namespace{
                 rv::transform(
                     [&ary,col,row](auto&& tup) {
                         auto [col_offset, row_offset] = tup;
-                        auto vd = viewing_distance(ary, col, row, col_offset, row_offset);
-                        //std::cout << vd << "\n";
-                        return vd;
+                        return viewing_distance(ary, col, row, col_offset, row_offset);
                     }
                 ),
             1,
@@ -230,16 +183,6 @@ namespace{
 
 void aoc::day_8(int day, const std::string& title) {
     auto input_strings = file_to_string_vector(input_path(day, 1));
-    
-    /*
-    auto input_strings = std::vector<std::string>{
-        "30373",
-        "25512",
-        "65332",
-        "33549",
-        "35390"
-    };
-    */
     auto input = strings_to_2D_array_of_digits(input_strings);
 
     std::cout << header(day, title);
