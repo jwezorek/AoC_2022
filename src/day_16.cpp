64006e826c18212f2d370c6eee9b3f0a254dd6df
Joe Wezorek <jwezorek@gmail.com>
2022-12-16 20:18:08 -0800

    part 1, fast

--- src/day_16.cpp
+++ src/day_16.cpp
@@ -80,9 +80,94 @@ namespace {
         };
     }
 
+    int length_of_shortest_path(const graph& g, int src, int dest) {
+        std::unordered_map<int, int> distance;
+        std::function<void(int, int)> dfs;
+        dfs = [&](int u, int depth)->void {
+            if (distance.contains(u)) {
+                if (depth < distance[u]) {
+                    distance[u] = depth;
+                }
+                return;
+            }
+            distance[u] = depth;
+            if (u == dest) {
+                return;
+            }
+            for (int v : g.verts[u].neighbors |  rv::transform(  [](auto&& e) { return e.dest;  } )) {
+                dfs(v, depth + 1);
+            }
+            return;
+        };
+        dfs(src, 0);
+        return distance[dest];
+    }
+
+    std::vector<std::vector<int>> shortest_path_lengths(const graph& g) {
+        int n = static_cast<int>(g.verts.size());
+        std::vector<std::vector<int>> tbl(n, std::vector<int>(n, 0));
+        for (auto [u, v] : rv::cartesian_product(rv::iota(0, n), rv::iota(0, n))) {
+            tbl[u][v] = length_of_shortest_path(g, u, v);
+            tbl[v][u] = tbl[u][v];
+        }
+        return tbl;
+    }
+
+    graph build_weighted_graph(const graph& g, std::vector<std::vector<int>>& dist) {
+        std::unordered_map<int, int> new_index_to_old_index =
+            rv::enumerate(
+                g.verts |
+                rv::remove_if(
+                    [&](auto&& u) {
+                        return u.flow == 0 && u.index != g.start;
+                    }
+                )
+            ) | rv::transform(
+                [](auto&& i_u)->std::unordered_map<int, int>::value_type {
+                    auto [i, u] = i_u;
+                    return { static_cast<int>(i), u.index };
+                }
+            ) | r::to< std::unordered_map<int, int>>();
+        int n = static_cast<int>(new_index_to_old_index.size());
+
+        graph weighted_graph;
+        for (int new_index : rv::iota(0, n)) {
+            std::vector<edge> edges;
+            int old_index = new_index_to_old_index[new_index];
+            for (int v : rv::iota(0, n)) {
+                if (v == new_index) {
+                    continue;
+                }
+                if (g.verts[new_index_to_old_index[v]].flow == 0) {
+                    continue;
+                }
+                edges.push_back( 
+                    edge {
+                        dist[old_index][new_index_to_old_index[v]],
+                        v
+                    }
+                );
+            }
+            const auto& old_vert = g.verts[old_index];
+            if (old_vert.label == "AA") {
+                weighted_graph.start = new_index;
+            }
+            weighted_graph.verts.push_back(
+                vertex{
+                    .label = old_vert.label,
+                    .index = new_index,
+                    .flow = old_vert.flow,
+                    .neighbors = edges
+                }
+            );
+        }
+        return weighted_graph;
+    }
+
     struct traversal_state {
         uint64_t open_valves;
         int location;
+        int elephant_location;
         int minutes_elapsed;
         int total_flow;
 
@@ -117,94 +202,52 @@ namespace {
 
     using state_set = std::unordered_set<traversal_state, state_hasher>;
 
-    struct move {
-        bool open_valve;
-        int dest;
-    };
-
-    struct loc_and_valve_state {
-        int loc;
-        uint64_t valves;
-
-        bool operator==(const loc_and_valve_state& lvs) const {
-            return loc == lvs.loc && valves == lvs.valves;
-        }
-    };
-
-    struct loc_and_valve_state_hasher {
-        size_t operator()(const loc_and_valve_state& s) const {
-            size_t seed = 0;
-            boost::hash_combine(seed, s.loc);
-            boost::hash_combine(seed, s.valves);
-            return seed;
-        }
-    };
-
-    using loc_and_valve_map = 
-        std::unordered_map<loc_and_valve_state, std::map<int, int>, loc_and_valve_state_hasher>;
-
-    std::vector<move> legal_moves(const graph& g, const traversal_state& state) {
+    std::vector<edge> neighbors(const graph& g, const traversal_state& state, int max_time) {
         auto u = state.location;
-        bool is_valve_open = state.is_valve_open(u);
-        if (!is_valve_open && g.verts[u].flow > 0) {
-            return rv::concat(
-                rv::single(move{ true, -1 }),
-                g.verts[u].neighbors | rv::transform([&](edge e)->move {  return { false, e.dest}; })
-            ) | r::to_vector;
-        }
         return g.verts[u].neighbors |
-            rv::transform([&](auto&& e)->move {  return { false, e.dest }; }) | r::to_vector;
+            rv::remove_if(
+                [&](auto&& e)->bool {
+                    int v = e.dest;
+                    if (g.verts[v].flow == 0) {
+                        return true;
+                    }
+                    if (state.is_valve_open(v)) {
+                        return true;
+                    }
+                    if (state.minutes_elapsed + e.weight >= max_time) {
+                        return true;
+                    }
+                    return false;
+                }
+        ) | r::to_vector;
     }
 
-    void augment_flow(const graph& g, traversal_state& state) {
+    int current_flow(const graph& g, const traversal_state& state) {
+        int total_flow = 0;
         for (const auto& v : g.verts) {
             if (state.is_valve_open(v.index)) {
-                state.total_flow += v.flow;
+                total_flow += v.flow;
             }
         }
+        //std::cout << "total flow: " << total_flow << "\n";
+        return total_flow;
     }
 
-    traversal_state make_move(const graph& g, const traversal_state& state, const move& m) {
-        auto new_state = state;
-        augment_flow(g, new_state);
-        if (m.open_valve) {
-            new_state.open_valve(new_state.location);
-        } else {
-            new_state.location = m.dest;
-        }
-        new_state.minutes_elapsed++;
+    traversal_state make_move(const graph& g, const traversal_state& state, const edge& e) {
+        traversal_state new_state = state;
+        auto [distance, dest] = e;
+        int elapsed_time = distance + 1;
+        new_state.minutes_elapsed += elapsed_time;
+        new_state.total_flow += elapsed_time * current_flow(g, state);
+        new_state.open_valve(dest);
+        new_state.location = dest;
         return new_state;
     }
 
-    using traversal_stack_item = std::tuple<traversal_state, move>;
+    using traversal_stack_item = std::tuple<traversal_state, edge>;
 
-    using time_table = std::map<int, int>;
-
-    bool check_time_table(time_table& tbl, int time, int val) {
-        if (tbl.empty()) {
-            tbl[time] = val;
-            return true;
-        }
-        auto i = tbl.lower_bound(time);
-        decltype(i) j;
-        if (i == tbl.end() || i->first != time) {
-            j = std::prev(i);
-        } else {
-            j = i;
-        }
-        if (j != tbl.end() && val <= j->second) {
-            return false;
-        }
-        if (i != tbl.end()) {
-            tbl.erase(i, tbl.end());
-        }
-        tbl[time] = val;
-        return true;
-    }
-
-    int do_traversal(const graph& g) {
+    int do_traversal(const graph& g, int max_time) {
         std::stack<traversal_stack_item> stack;
-        loc_and_valve_map max_flow_at_time;
         state_set ss;
         traversal_state state{
             .open_valves = 0,
@@ -212,49 +255,88 @@ namespace {
             .minutes_elapsed = 0,
             .total_flow = 0
         };
-        for (auto&& m : legal_moves(g, state)) {
-            stack.push({ state, m });
+        for (auto&& e : neighbors(g, state, max_time)) {
+            stack.push({ state, e });
         }
         int max_flow = 0;
-        max_flow_at_time[loc_and_valve_state{ state.location, 0 }][0] = 0;
         while (!stack.empty()) {
-            auto [state, move] = stack.top();
+            auto [state, e] = stack.top();
             stack.pop();
-            std::cout << stack.size() << " : " << max_flow << " " << state.minutes_elapsed << " " << state.open_valves << " " << state.location << "\n";
-            auto new_state = make_move(g, state, move);
+            //std::cout << stack.size() << " : " << max_flow << " " << state.minutes_elapsed << " " << state.open_valves << " " << state.location << "\n";
+            auto new_state = make_move(g, state, e);
 
-            if (ss.contains(new_state)) {
-                continue;
-            }
-            ss.insert(new_state);
-
-            time_table& tbl = max_flow_at_time[loc_and_valve_state{ new_state.location,new_state.open_valves }];
-            if (!check_time_table(tbl, new_state.minutes_elapsed, new_state.total_flow)) {
-                continue;
-            }
-
-            if (new_state.minutes_elapsed == 30) {
+            if (new_state.minutes_elapsed == max_time) {
+                //std::cout << "foo:" << new_state.minutes_elapsed << "\n";
                 max_flow = std::max(max_flow, new_state.total_flow);
                 continue;
             }
-            for (auto&& m : legal_moves(g, new_state)) {
+
+            auto neigh = neighbors(g, new_state, max_time);
+            if (neigh.empty()) {
+                auto flow = new_state.total_flow + (max_time - new_state.minutes_elapsed) * current_flow(g, new_state);
+                max_flow = std::max(max_flow, flow);
+                continue;
+            }
+            for (auto&& m : neigh) {
                 stack.push({ new_state, m });
             }
         }
         return max_flow;
     }
 
+    using traversal_stack_item_with_elphant = std::tuple<traversal_state, edge, edge>;
+    /*
+    int do_traversal_with_elephant(const graph& g, int max_time) {
+        std::stack<traversal_stack_item_with_elphant> stack;
+        state_set ss;
+        traversal_state state{
+            .open_valves = 0,
+            .location = g.start,
+            .elephant_location = g.start,
+            .minutes_elapsed = 0,
+            .total_flow = 0
+        };
+        for (auto&& e : neighbors(g, state, max_time)) {
+            stack.push({ state, e });
+        }
+        int max_flow = 0;
+        while (!stack.empty()) {
+            auto [state, e] = stack.top();
+            stack.pop();
+            //std::cout << stack.size() << " : " << max_flow << " " << state.minutes_elapsed << " " << state.open_valves << " " << state.location << "\n";
+            auto new_state = make_move(g, state, e);
+
+            if (new_state.minutes_elapsed == max_time) {
+                //std::cout << "foo:" << new_state.minutes_elapsed << "\n";
+                max_flow = std::max(max_flow, new_state.total_flow);
+                continue;
+            }
+
+            auto neigh = neighbors(g, new_state, max_time);
+            if (neigh.empty()) {
+                auto flow = new_state.total_flow + (max_time - new_state.minutes_elapsed) * current_flow(g, new_state);
+                max_flow = std::max(max_flow, flow);
+                continue;
+            }
+            for (auto&& m : neigh) {
+                stack.push({ new_state, m });
+            }
+        }
+        return max_flow;
+    }
+    */
 }
 
 /*------------------------------------------------------------------------------------------------*/
 
 void aoc::day_16(const std::string& title) {
-    auto input = file_to_string_vector(input_path(16, {}));
+    auto input = file_to_string_vector(input_path(16, 1));
     auto g = build_graph(input);
 
-    int foo = do_traversal(g);
+    auto tbl = shortest_path_lengths(g);
+    g = build_weighted_graph(g, tbl);
 
     std::cout << header(16, title);
-    std::cout << "  part 1: " << foo << "\n";
+    std::cout << "  part 1: " << do_traversal(g, 30) << "\n";
     std::cout << "  part 2: " << 0 << "\n";
 }
